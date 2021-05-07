32bb93dc660f2109eabf7b7ffb15bed04d5acc2e
YanchangWang <759735578@qq.com>
2021-05-07 14:54:09 +0800

    快捷菜单

---
+++ server/ruoyi-system/src/main/java/com/ruoyi/wechat/category/mapper/ShortcutMenuMapper.java
@@ -0,0 +1,61 @@
+package com.ruoyi.wechat.category.mapper;
+
+import com.ruoyi.wechat.category.domain.ShortcutMenu;
+import java.util.List;
+
+/**
+ * 二级类的快捷菜单关联增删查改Mapper接口
+ * 
+ * @author ruoyi
+ * @date 2021-05-06
+ */
+public interface ShortcutMenuMapper 
+{
+    /**
+     * 查询二级类的快捷菜单关联增删查改
+     * 
+     * @param shortcutMenuId 二级类的快捷菜单关联增删查改ID
+     * @return 二级类的快捷菜单关联增删查改
+     */
+    public ShortcutMenu selectShortcutMenuById(Long shortcutMenuId);
+
+    /**
+     * 查询二级类的快捷菜单关联增删查改列表
+     * 
+     * @param shortcutMenu 二级类的快捷菜单关联增删查改
+     * @return 二级类的快捷菜单关联增删查改集合
+     */
+    public List<ShortcutMenu> selectShortcutMenuList(ShortcutMenu shortcutMenu);
+
+    /**
+     * 新增二级类的快捷菜单关联增删查改
+     * 
+     * @param shortcutMenu 二级类的快捷菜单关联增删查改
+     * @return 结果
+     */
+    public int insertShortcutMenu(ShortcutMenu shortcutMenu);
+
+    /**
+     * 修改二级类的快捷菜单关联增删查改
+     * 
+     * @param shortcutMenu 二级类的快捷菜单关联增删查改
+     * @return 结果
+     */
+    public int updateShortcutMenu(ShortcutMenu shortcutMenu);
+
+    /**
+     * 删除二级类的快捷菜单关联增删查改
+     * 
+     * @param shortcutMenuId 二级类的快捷菜单关联增删查改ID
+     * @return 结果
+     */
+    public int deleteShortcutMenuById(Long shortcutMenuId);
+
+    /**
+     * 批量删除二级类的快捷菜单关联增删查改
+     * 
+     * @param shortcutMenuIds 需要删除的数据ID
+     * @return 结果
+     */
+    public int deleteShortcutMenuByIds(Long[] shortcutMenuIds);
+}
