13d95e7a22201c86defa17695e30cb400b15d535
lzj <17639254326@163.com>
2021-05-11 10:59:35 +0800

    sql

--- server/sql/zuihouyunxing.sql
+++ server/sql/zuihouyunxing.sql
@@ -16,7 +16,7 @@
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
-123
+
 -- ----------------------------
 -- Table structure for signin_time
 -- ----------------------------
@@ -644,3 +644,38 @@ values('签到时间删除', @parentId, '4',  '#', '', 1, 0, 'F', '0', '0', 'sys
 
 insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
 values('签到时间导出', @parentId, '5',  '#', '', 1, 0, 'F', '0', '0', 'system:time:export',       '#', 'admin', sysdate(), '', null, '');
+
+-- ----------------------------
+-- Table structure for user_msg
+-- ----------------------------
+
+CREATE TABLE `user_msg` (
+  `user_id` int(11) NOT NULL COMMENT '用户ID',
+  `user_msg` varchar(255) DEFAULT NULL COMMENT '发送给用户的消息',
+  `time` datetime DEFAULT NULL COMMENT '发送消息的时间',
+  `title` varchar(255) DEFAULT NULL COMMENT '消息标题',
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
+
+-- 菜单 SQL
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
+values('消息列表', '2006', '1', 'message', 'system/message/index', 1, 0, 'C', '0', '0', 'system:message:list', '#', 'admin', sysdate(), '', null, '消息列表菜单');
+
+-- 按钮父菜单ID
+SELECT @parentId := LAST_INSERT_ID();
+
+-- 按钮 SQL
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
+values('消息列表查询', @parentId, '1',  '#', '', 1, 0, 'F', '0', '0', 'system:message:query',        '#', 'admin', sysdate(), '', null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
+values('消息列表新增', @parentId, '2',  '#', '', 1, 0, 'F', '0', '0', 'system:message:add',          '#', 'admin', sysdate(), '', null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
+values('消息列表修改', @parentId, '3',  '#', '', 1, 0, 'F', '0', '0', 'system:message:edit',         '#', 'admin', sysdate(), '', null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
+values('消息列表删除', @parentId, '4',  '#', '', 1, 0, 'F', '0', '0', 'system:message:remove',       '#', 'admin', sysdate(), '', null, '');
+
+insert into sys_menu (menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_by, create_time, update_by, update_time, remark)
+values('消息列表导出', @parentId, '5',  '#', '', 1, 0, 'F', '0', '0', 'system:message:export',       '#', 'admin', sysdate(), '', null, '');
